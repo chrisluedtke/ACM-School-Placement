3270a82aebb819f25ea8417a55a5fd4a1766e1b3
Alex Perusse <perusse.a@gmail.com>
2017-06-01 11:42:32 -0700

    added PBI script version

---
+++ acm_placement_pbi.R
@@ -0,0 +1,6 @@
+# ACM Placement Algorithem
+# v0.9
+# Developed by Alex Perusse and Chris Luedtke
+#
+# The purpose of this version is to have a single script which can be run in its entireity in approximately
+# the same way that the Power BI version will. 
