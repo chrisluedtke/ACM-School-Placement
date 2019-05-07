944af2a47202abadf41932c974abdfc10c0b1361
Alex Perusse <perusse.a@gmail.com>
2019-05-07 12:15:28 -0700

    the build is running again

--- build acm placement.bat
+++ build acm placement.bat
@@ -1,7 +1,7 @@
 @echo off
 :: Used to pull a fresh image from Dockerhub
-docker build ./webapp/ -t acmplacement
+docker build ./webapp/ -t mrklees/acmplacement:latest
 :: Finally, conveniently open chrome to localhost at the target port.
 start chrome --new-window "http://localhost:8000"
 :: Then run the image. -p 8000:8000 exposes the 8000 port so that we can see the app in the browser.
-docker run -it -p 8000:8000 acmplacement
+docker run -it -p 8000:8000 mrklees/acmplacement:latest
--- launch acm placement - old.bat
+++ launch acm placement - old.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Used to pull a fresh image from Dockerhub
-docker pull mrklees/acmplacement:latest
+docker pull cleudtke/acmplacement
 :: Finally, conveniently open chrome to localhost at the target port.
 start chrome --new-window "http://localhost:8000"
 :: Then run the image. -p 8000:8000 exposes the 8000 port so that we can see the app in the browser.
