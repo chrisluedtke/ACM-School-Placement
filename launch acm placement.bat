07d41ee3db163329f05dc5ba20872455c8a008a2
Alex Perusse <perusse.a@gmail.com>
2019-05-15 12:03:36 -0700

    updated launch script

--- launch acm placement.bat
+++ launch acm placement.bat
@@ -4,4 +4,4 @@ docker pull mrklees/acmplacement:latest
 :: Finally, conveniently open chrome to localhost at the target port.
 start chrome --new-window "http://localhost:8000"
 :: Then run the image. -p 8000:8000 exposes the 8000 port so that we can see the app in the browser.
-docker run -it -p 8000:8000 cluedtke/acmplacement
+docker run -it -p 8000:8000 mrklees/acmplacement:latest
